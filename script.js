5b9964bfc8d50f95c270c0ce5e60f8fa3a1627e1
Saurav Kumar Jha <122507083+jhajee22@users.noreply.github.com>
2023-09-29 01:31:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function calculateMinCost() {
   //your code here
+	 const ropeLengthsInput = document.getElementById("rope-lengths");
+
+    // Get the comma-separated input string and split it into an array of integers
+    const inputStr = ropeLengthsInput.value;
+    const inputArr = inputStr.split(",").map(Number);
+
+    // Calculate the minimum cost using the provided minCostOfRopes function
+    const result = minCostOfRopes(inputArr);
+
+    // Display the result in the <div id="result"> element
+    const resultDiv = document.getElementById("result");
+    resultDiv.textContent = `Minimum Cost: ${result}`;
   
   
   
